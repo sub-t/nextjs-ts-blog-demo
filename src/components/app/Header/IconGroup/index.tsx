03de6bdf53e56fd5b8724ec2b75cb1128029fb96
sub-t <subt@outlook.jp>
2022-06-12 18:11:19 +0900

    Revert "[fix] aria-labelの付与、不要なファイルの削除"
    This reverts commit de679c94abb7a8364b4bfcde7fe0f6dbb974a5a3.

--- src/components/app/Header/IconGroup/index.tsx
+++ src/components/app/Header/IconGroup/index.tsx
@@ -5,17 +5,17 @@ import { OuterLink } from '@/components/common/Link';
 
 export const IconGroup = () => (
   <Wrap gap="5">
-    <OuterLink aria-label="github" href="https://github.com/">
+    <OuterLink href="https://github.com/">
       <Icon size="2">
         <FaGithub />
       </Icon>
     </OuterLink>
-    <OuterLink aria-label="twitter" href="https://twitter.com/">
+    <OuterLink href="https://twitter.com/">
       <Icon size="2">
         <FaTwitter />
       </Icon>
     </OuterLink>
-    <OuterLink aria-label="instagram" href="https://www.instagram.com/">
+    <OuterLink href="https://www.instagram.com/">
       <Icon size="2">
         <FaInstagram />
       </Icon>
