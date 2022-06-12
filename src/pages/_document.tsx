a6282a7fb19aea0e1d5dfbbf2405a484f5afb7f4
sub-t <subt@outlook.jp>
2022-06-12 11:12:46 +0900

    [test] favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,19 +10,20 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        <link rel="shortcut icon" type="image/x-icon" href="/static/favicons/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/static/favicons/apple-touch-icon.png"
         />
         <link
-          rel="icon"
+          rel="shortcut icon"
           type="image/png"
           sizes="32x32"
           href="/static/favicons/favicon-32x32.png"
         />
         <link
-          rel="icon"
+          rel="shortcut icon"
           type="image/png"
           sizes="16x16"
           href="/static/favicons/favicon-16x16.png"
